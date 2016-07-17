1eef60d2406eee7f2dbc26a30ae63c4ae548f1dc
darinmarmol <daring_185@hotmail.com>
2016-07-16 19:54:52 -0600

    Update principal.blade.php

--- Calendario/resources/views/admin/templete/principal.blade.php
+++ Calendario/resources/views/admin/templete/principal.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+	{!!Html::style('css/bootstrap.min.css')!!}
+	{!!Html::style('css/metismenu.min.css')!!}
+	{!!Html::style('css/sb-admin-2.css')!!} 
+	{!!Html::style('css/font-awesome.min.css')!!} 
+	{!!Html::style('css/estilos.css')!!} 
+
+
 	<title>('title','Default') | panel de administracion</title>
 	<link rel="stylesheet"  href="{{asset('plugins/bootstrap/css/bootstrap.css')}}">
 </head>
@@ -9,5 +17,12 @@
 		
 	</section>
 @yield('content')
+
+
+	{!!Html::script('js/query.min.js')!!}
+	{!!Html::script('js/bootstrap.min.js')!!}
+	{!!Html::script('js/metismenu.min.js')!!}
+	{!!Html::script('js/sb-admin-2.js')!!} 
+</body>
 </body>
 </html>
